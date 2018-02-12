98c22713e9569b97cb7eaf62eb8927b98304b1a2
kalle (jag) <kalle@jillheden.se>
2018-02-12 15:16:43 +0100

    Setup babel for some slick ES6+ functionality

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-const express = require('express');
+import express from 'express';
 
 const app = express();
 const port = 8080;
 
+app.use(express.static('dist/public'));
 app.listen(port);
 
 console.log(`Node server kör på http://localhost:${port}`);
